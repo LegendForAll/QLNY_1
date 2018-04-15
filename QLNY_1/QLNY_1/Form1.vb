19767a39862c7029890768fb4344accfa41a01c7
LegendForAll <34824859+LegendForAll@users.noreply.github.com>
2018-04-15 12:21:47 +0700

    Update_text

--- QLNY_1/QLNY_1/Form1.vb
+++ QLNY_1/QLNY_1/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+    'text Sync github
     Private Sub THEMNYToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles THEMNYToolStripMenuItem.Click
         Dim frm As Them_NG = New Them_NG()
         frm.MdiParent = Me
